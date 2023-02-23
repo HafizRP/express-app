cc0e9e17ba2c23bbe1d80fdbbc17efb5e0a3bbb0
HafizRP <my081381350574@gmail.com>
2023-02-23 20:32:35 +0700

    add models

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,22 +1,23 @@
+// Dummy DB
 const users = [{ username: "SIJA", password: "sijasmkn69jkt" }];
 
+// Fungsi Login/Sign In
 exports.signin = (req, res) => {
   try {
-    // Check username is valid
+    // Check apakah user ada/tidak
     const user = users.find((user) => user.username == req.body.username);
     if (!user) {
       res.status(404).send("Username tidak ditemukan");
     }
 
-    // Check password is valid
+    // Check apabila user tersedia, apakah password valid/tidak
     if (user.password != req.body.password) {
       res.status(400).send("Password Invalid");
     }
 
-    // When password and username valid
-    res.send({ user: user });
+    // mengirimkan user ketika valid
+    res.send({ user });
   } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
+    res.status(500).json(error);
   }
 };
